f168cb234eb86d6560bb831144e1036077d0cfa4
Alexander Pensavalle <alexanderpensavalle@Alexanders-MacBook-Pro.local>
2020-09-01 13:36:20 -0400

    Jim's step 6

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Courtney's Yoga Studio" });
 });
 
 module.exports = router;
